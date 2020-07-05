d19339c67fc98feb9d8a911463058688e5556192
rishu3895 <rishabh.rsharma1999@gmail.com>
2020-07-05 17:48:55 +0530

    completed the html and css

---
+++ script/script.js
@@ -0,0 +1,6 @@
+let container=document.getElementsByClassName('container');
+function makeDiv(){
+    let newDiv=document.createElement('div');
+    newDiv.innerHTML='hey people';
+    document.container.appendChild(newDiv); 
+}
